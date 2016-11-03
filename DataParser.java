a99323cda97b506120ac1a4ba23ee5b75540346a
Heng Li <lihengokok@gmail.com>
2016-11-03 13:03:14 -0400

    implement parser

--- DataParser.java
+++ DataParser.java
@@ -1,15 +1,54 @@
 // Parse input data xxx.tsp into a 2D-array of double
+import java.util.Scanner;
+import java.io.*;
 class DataParser {
 	/**
-	 * @param: String filename
+	 * @param: String filename "/Data/xxx.tsp"
 	 * @return: double[] 2D-array of double
 	 */
-	public static double[][] parse(String fileName) {
-		
-		return new double[1];
+	public static double[][] parse(String filename) throws FileNotFoundException {
+		// Init file
+		File inputFile = new File(filename);
+		Scanner sc = new Scanner(inputFile);
+
+		// Skip 2 lines
+		String name = sc.nextLine();
+		String comment = sc.nextLine();
+		String dimension = sc.nextLine();
+
+		// parse dimension
+		int size = parseDimension(dimension);
+
+		// Skip 2 lines
+		String weightType = sc.nextLine();
+		String nodeSection = sc.nextLine();
+
+		// Read node list
+		double[] nodeList = new double[size * 2];
+		for (int i = 0; i < size; i++) {
+			String[] splitedLine = sc.nextLine().split(" ");
+			nodeList[i * 2] = Double.parseDouble(splitedLine[1]);
+			nodeList[i * 2 + 1] = Double.parseDouble(splitedLine[2]);
+		}
+		return parseNodesTo2DArray(nodeList, size);
 	}
 
-	public static double[][] parseNodesTo2DArray(double[] nodes) {
-		return double[1][1];
+	private static double[][] parseNodesTo2DArray(double[] nodes, int size) {
+		double[][] nodesMap = new double[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				nodesMap[i][j] = getEucDis(nodes[i * 2], nodes[i * 2 + 1], nodes[j * 2], nodes[j * 2 + 1]);
+			}
+		}
+		return nodesMap;
+	}
+
+	private static double getEucDis(double x1, double y1, double x2, double y2) {
+		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	}
+
+	private static int parseDimension(String line) {
+		String num = line.split(": ")[1];
+		return Integer.parseInt(num);
 	}
 }
--- TSP.java
+++ TSP.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+import java.io.*;
 class TSP {
-	public static void main(String args[]) {
-		return 0;
+	public static void main(String args[]) throws FileNotFoundException {
+		double[][] newMap = DataParser.parse("./DATA/Boston.tsp");
 	}
 }
