270515014ea804a9ee8fa9e5ab481f70aa0190fa
Heng Li <lihengokok@gmail.com>
2016-11-03 12:22:06 -0400

    add parser for input data, add main file

---
+++ DataParser.java
@@ -0,0 +1,10 @@
+// Parse input data xxx.tsp into a 2D-array of double
+class DataParser {
+	/**
+	 * @param: String filename
+	 * @return: double[] 2D-array of double
+	 */
+	public static double[] parse(String ) {
+		return new double[1];
+	}
+}
---
+++ TSP.java
@@ -0,0 +1,5 @@
+class TSP {
+	public static void main(String args[]) {
+		return 0;
+	}
+}
