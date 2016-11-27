38fa881b2de0973659ec735557da48bde20041ef
Heng Li <lihengokok@gmail.com>
2016-11-27 17:15:41 -0500

    yolo

--- TSPUtil.java
+++
@@ -1,5 +0,0 @@
-class TSPUtil {
-	public static int getEucDis(double x1, double y1, double x2, double y2) {
-		return (int) Math.round(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
-	}
-}
